91d7721280ceb5436bcb1040347f16b5e6134e17
Laurie Young <laurie@eaglet.config>
2009-09-22 22:53:16 +0100

    Removed hard coded calendar link, now set via an environment var

--- masterplan.rb
+++ masterplan.rb
@@ -7,31 +7,31 @@ require 'rio'
 require 'erb'
 
 get '/' do
-  file = RIO.rio("https://www.google.com/calendar/ical/dance%40wildfalcon.com/private-487b33af7da6bc2ab99e9c93b1d1d4ea/basic.ics")
-  today = Time.now
-  @days = {}
-  (0..180).each do |d|
-    date = today+d.days
-    @days["#{date.year}#{date.yday}"]={:date => date}
-  end
-  
-  "Hello"
-    # Parser returns an array of calendars because a single file
-    # can have multiple calendars.
-    cals = Icalendar.parse(file.read)
-    cal = cals.first
-    
-    cal.events.each do |event|
+  ical_filename = ENV['MASTERPLAN_FILENAME']
+
+  if ical_filename
+    file = RIO.rio(ical_filename)
+    today = Time.now
+    @days = {}
+    (0..180).each do |d|
+      date = today+d.days
+      @days["#{date.year}#{date.yday}"]={:date => date}
+    end
+
+    Icalendar.parse(file.read).first.events.each do |event|
       date = event.dtstart
       if @days["#{date.year}#{date.yday}"]
         @days["#{date.year}#{date.yday}"][:events] ||= []
         @days["#{date.year}#{date.yday}"][:events] << event
       end
     end
-    
+
     @days = @days.to_a.map{|t| t.last}
 
     @days.sort!{|a,b| a[:date] <=> b[:date]}
-    
+
     erb :calendar
+  else
+    'Please set the environment var "masterplan_filename"'
+  end
 end
