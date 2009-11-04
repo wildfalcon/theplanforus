e69318ca3a5e18caefbcb054136fe1452c45f821
Wildfalcon <laurie@wildfalcon.com>
2009-11-04 22:21:43 +0000

    Removed commented old version of events controller, tidied plans controller
    This is safe as its now covered by resources_controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,73 +1,51 @@
 class PlansController < ApplicationController
-  # GET /plans
-  # GET /plans.xml
+
   def index
     @plans = Plan.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @plans }
-    end
   end
 
-  # GET /plans/1
-  # GET /plans/1.xml
+  def weekly
+    @plan = Plan.find(params[:id])
+  end
+
+  def daily
+    @plan = Plan.find(params[:id])
+  end
+
   def show
     @plan = Plan.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @plan }
-    end
   end
 
-  # GET /plans/new
-  # GET /plans/new.xml
+
   def new
     @plan = Plan.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @plan }
-    end
   end
 
-  # GET /plans/1/edit
+
   def edit
     @plan = Plan.find(params[:id])
   end
 
-  # POST /plans
-  # POST /plans.xml
+
   def create
     @plan = Plan.new(params[:plan])
-
-    respond_to do |format|
-      if @plan.save
-        flash[:notice] = 'Plan was successfully created.'
-        format.html { redirect_to(@plan) }
-        format.xml  { render :xml => @plan, :status => :created, :location => @plan }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @plan.errors, :status => :unprocessable_entity }
-      end
+    if @plan.save
+      flash[:notice] = 'Plan was successfully created.'
+      redirect_to(@plan) 
+    else
+      render :action => "new" 
     end
+
   end
 
-  # PUT /plans/1
-  # PUT /plans/1.xml
+
   def update
     @plan = Plan.find(params[:id])
-
-    respond_to do |format|
-      if @plan.update_attributes(params[:plan])
-        flash[:notice] = 'Plan was successfully updated.'
-        format.html { redirect_to(@plan) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @plan.errors, :status => :unprocessable_entity }
-      end
+    if @plan.update_attributes(params[:plan])
+      flash[:notice] = 'Plan was successfully updated.'
+      redirect_to(@plan) 
+    else
+      render :action => "edit" 
     end
   end
 
@@ -76,10 +54,7 @@ class PlansController < ApplicationController
   def destroy
     @plan = Plan.find(params[:id])
     @plan.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(plans_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(plans_url)
   end
+
 end
