2e104cab43ff23e7af410259b6886db431b8ac8f
Wildfalcon <laurie@wildfalcon.com>
2009-11-11 00:02:10 +0000

    Added an event detail pannel

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,18 @@ class Event < ActiveRecord::Base
   belongs_to :plan
   
   default_scope :order  => "date"
+  named_scope :primary, :conditions => {:primary => true} 
+  named_scope :before, lambda { |date| { :conditions => ['date < ?', date] }}
   
   def summary
     name
+  end
+  
+  def preceeding_lessons
+    plan.lessons.before(self.date)
+  end
+  
+  def preceeding_events
+    plan.events.before(self.date)
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,6 @@
 class Lesson < ActiveRecord::Base
   belongs_to :plan
+  
+  named_scope :before, lambda { |date| { :conditions => ['date < ?', date] }}
+  
 end
