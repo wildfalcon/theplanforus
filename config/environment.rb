57683d1b4f9d114968bf4123af0b12d58daa6746
Wildfalcon <laurie@wildfalcon.com>
2009-11-11 18:30:29 +0000

    installed authlogic

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,10 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rio"
   config.gem "icalendar"
+  config.gem "authlogic"
+  
   config.gem "factory_girl", :source => "https://gemcutter.org"
+  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
