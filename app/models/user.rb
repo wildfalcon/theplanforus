30ba88d3f872f19787bf8c5313a394af24a686ed
Wildfalcon <laurie@wildfalcon.com>
2009-11-17 15:22:26 +0000

    Added actionMailer integration to password reset process

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
 
   def deliver_password_reset_instructions!  
     reset_perishable_token!  
-    # Notifier.deliver_password_reset_instructions(self)  
+    Notifier.deliver_password_reset_instructions(self)  
   end
 end
