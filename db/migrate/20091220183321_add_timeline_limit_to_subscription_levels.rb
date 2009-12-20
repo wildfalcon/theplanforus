7807cfb486fd29cdca7484d2b404df5e985dbe78
Wildfalcon <laurie@wildfalcon.com>
2009-12-20 19:13:01 +0000

    Fixed migration to save

--- db/migrate/20091220183321_add_timeline_limit_to_subscription_levels.rb
+++ db/migrate/20091220183321_add_timeline_limit_to_subscription_levels.rb
@@ -1,7 +1,7 @@
 class AddTimelineLimitToSubscriptionLevels < ActiveRecord::Migration
   def self.up
      add_column :subscription_levels, :timeline_limit, :integer
-     SubscriptionLevel.all.each{|s|  s.timeline_limit=30}
+     SubscriptionLevel.all.each{|s|  s.timeline_limit=30; s.save}
   end
 
   def self.down
