d3b088bd3f5f3437e9cf5f8095993762146225cd
Wildfalcon <laurie@wildfalcon.com>
2009-12-30 18:03:04 +0000

    Made lessons and events list only show upcoming events

--- app/models/preparation.rb
+++ app/models/preparation.rb
@@ -1,6 +1,7 @@
 class Preparation < ActiveRecord::Base
   belongs_to :plan
   
+  default_scope :order  => "date"
   named_scope :upcoming, :conditions => ['date > ?', Time.now]
   named_scope :before, lambda { |date| { :conditions => ['date < ?', date] }}
   
